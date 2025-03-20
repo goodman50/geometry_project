420c59cb25e46466826a32ca5bcc29dbc0a64621
Lee L Goodman <121632235+goodman50@users.noreply.github.com>
2025-03-20 12:23:16 -0400

    Add files via upload

---
+++ geometry_test.adb
@@ -0,0 +1,25 @@
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+with geometry; use geometry;
+
+procedure Geometry_Test is
+    -- procedure put_float(number : Float) is begin
+    --    Float_Text_IO.put(Item => number, Aft => 3, Exp => 0);
+    -- end put_float;
+    cup : Geometry.Cylinder;
+    cup_volume : Float;
+    cup_surface_area : Float;
+
+begin
+    -- calculate volume of coffee mug
+    cup.radius := (3.25/2.0); -- 3.25 inches diameter
+    cup.height := (5.0); -- 5 inches tall
+    cup_volume := Geometry.volume (cup);
+    cup_surface_area := Geometry.surface_area (cup);
+
+    New_Line;
+    put("Cup Volume = " & cup_volume'Image & " inches");
+    New_Line;
+    put("Cup Surface Area = " & cup_surface_area'Image & " inches");
+end Geometry_Test;

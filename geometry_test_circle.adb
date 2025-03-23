a0d8649aef5373b782460bbbc238446e1e54f780
Lee L Goodman <121632235+goodman50@users.noreply.github.com>
2025-03-23 12:37:31 -0400

    Update geometry_test_circle.adb

--- geometry_test_circle.adb
+++ geometry_test_circle.adb
@@ -3,17 +3,17 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 with geometry; use geometry;
 
-procedure Geometry_Test_ircumference is
+procedure Geometry_Test_Circle is
     -- procedure put_float(number : Float) is begin
     --    Float_Text_IO.put(Item => number, Aft => 3, Exp => 0);
     -- end put_float;
-    cup : Geometry.Circumference;
+    cup : Geometry.Circle;
     cup_curcumference : Float;
     cup_surface_area : Float;
 
 begin
     -- calculate volume of coffee mug
-    cup.radius := (3.25/2.0); -- 3.25 inches diameter
+    cup.radius := 5.0; -- 5.0 inches diameter
     cup_curcumference := Geometry.curcumference (cup);
     cup_surface_area := Geometry.surface_area (cup);
 
@@ -21,4 +21,4 @@ begin
     put("Cup Curcumference = " & cup_curcumference'Image & " inches");
     New_Line;
     put("Cup Surface Area = " & cup_surface_area'Image & " inches");
-end Geometry_Test_Circumference;
+end Geometry_Test_Circle;
